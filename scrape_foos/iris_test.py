9957b5e4db7491eedf358ebdbdaa256480aa389e
Michael Nestel <nestelm@gmail.com>
2018-10-23 15:49:07 -0400

    another one

--- scrape_foos/iris_test.py
+++ scrape_foos/iris_test.py
@@ -1,4 +1,4 @@
 import pandas as pd
 data = [1,2,3,4,5]
-df = pd.DataFrame(data)
+df = pd.DataFrame(data, columns="test_column")
 df.to_gbq(project_id='scarlet-labs', destination_table='testing.iris_test', if_exists="replace")
