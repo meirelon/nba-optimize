08d4dc93ac2fd05439e8dff66944125c4b06e602
Michael Nestel <nestelm@gmail.com>
2018-10-23 15:47:41 -0400

    even easier

--- scrape_foos/iris_test.py
+++ scrape_foos/iris_test.py
@@ -1,5 +1,4 @@
-from sklearn.datasets import load_iris
 import pandas as pd
-
-df = pd.DataFrame(load_iris())
+data = [1,2,3,4,5]
+df = pd.DataFrame(data)
 df.to_gbq(project='scarlet-labs', destination_table='testing.iris_test', if_exists="replace")
