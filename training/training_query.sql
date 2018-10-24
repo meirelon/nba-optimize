877ab84dd642a844e2aea1ebaecac94f4d6e4ca3
Michael Nestel <nestelm@gmail.com>
2018-10-24 19:34:01 -0400

    updated training query

--- training/training_query.sql
+++ training/training_query.sql
@@ -2,15 +2,15 @@ with
 distinct_player_info AS(
 select player, bbrefID, pos
 from(
-select player, bbrefID, pos, g, MP, max(g) over(partition by player, bbrefID) as max_g, max(MP) over(partition by player, bbrefID) as max_mp
+select player, bbrefID, pos, g, secs_played, max(g) over(partition by player, bbrefID) as max_g, max(secs_played) over(partition by player, bbrefID) as max_secs_played
 from `scarlet-labs.basketball.playerinfo2018_20181024`
 where tm != 'TOT'
 )
-where g = max_g and MP = max_mp
+where g = max_g and secs_played = max_secs_played
 ),
 
 player_logs_full as(
-select date(timestamp(date)) as date, bbrefID, player, pos, tm, opp, G, GS, MP,
+select date(timestamp(date)) as date, bbrefID, player, pos, tm, opp, G, GS, secs_played,
 case when is_away is null then 'home' else 'away' end as is_away,
 plus_minus,
 dk
@@ -19,34 +19,55 @@ join(select * from distinct_player_info)
 using(bbrefID)
 ),
 
-opp_30d AS(
-select opp, pos, is_away, avg(dk) as dk_against_30d
-from player_logs_full
-where date between date_sub(date, interval 30 day) and date_sub(date, interval 1 day)
-group by 1,2,3
-),
-
-opp_7d AS(
-select opp, pos, is_away, avg(dk) as dk_against_7d
-from player_logs_full
-where date between date_sub(date, interval 8 day) and date_sub(date, interval 1 day)
-group by 1,2,3
-),
-
 todays_games as(
 select *
 from player_logs_full
 where date = date(timestamp('2018-01-15'))
 ),
 
+opp_30d AS(
+select opp, pos, avg(dk) as dk_against_30d
+from player_logs_full
+where date between date_sub(date(timestamp('2018-01-15')), interval 30 day) and date_sub(date(timestamp('2018-01-15')), interval 1 day)
+group by 1,2
+),
+
+opp_7d AS(
+select opp, pos, avg(dk) as dk_against_7d
+from player_logs_full
+where date between date_sub(date(timestamp('2018-01-15')), interval 8 day) and date_sub(date(timestamp('2018-01-15')), interval 1 day)
+group by 1,2
+),
+
 player_30d as(
-select bbrefID, avg(dk) as dk_30d,
+select bbrefID,
+avg(dk) as dk_30d,
+avg(secs_played) as secs_played_30d,
 sum(if(is_away = 'home', 1, 0)) as home_30d,
 sum(if(is_away = 'away', 1, 0)) as away_30d
 from player_logs_full
-where date between date_sub(date, interval 30 day) and date
+where date between date_sub(date(timestamp('2018-01-15')), interval 30 day) and date_sub(date(timestamp('2018-01-15')), interval 1 day)
+group by 1
+),
+
+player_7d as(
+select bbrefID,
+avg(dk) as dk_7d,
+avg(secs_played) as secs_played_7d,
+sum(if(is_away = 'home', 1, 0)) as home_7d,
+sum(if(is_away = 'away', 1, 0)) as away_7d
+from player_logs_full
+where date between date_sub(date(timestamp('2018-01-15')), interval 8 day) and date_sub(date(timestamp('2018-01-15')), interval 1 day)
 group by 1
 )
 
 select *
-from player_30d
+from todays_games
+left join(select * from player_30d)
+using(bbrefID)
+left join(select * from player_7d)
+using(bbrefID)
+left join(select * from opp_30d)
+using(opp, pos)
+left join(select * from opp_7d)
+using(opp, pos)
