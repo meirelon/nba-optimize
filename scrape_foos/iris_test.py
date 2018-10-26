13e6a18db66f56bd619d9334f1e7369ef08885c2
Michael Nestel <nestelm@gmail.com>
2018-10-26 11:02:38 -0400

    removing these foos

--- scrape_foos/iris_test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-data = [['Alex',10],['Bob',12],['Clarke',13]]
-df = pd.DataFrame(data,columns=['Name','Age'])
-df.to_gbq(project_id='scarlet-labs', destination_table='testing.iris_test', if_exists="replace")
-print("done!")
