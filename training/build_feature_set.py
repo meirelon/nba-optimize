9e484eb36df18174e0e258366659c88559d10ce8
meirelon <nestelm@gmail.com>
2018-10-31 23:14:35 -0400

    it was a pandas versioning error!

--- training/build_feature_set.py
+++ training/build_feature_set.py
@@ -30,7 +30,6 @@ class BuildFeatureSet:
 
     def get_feature_df(self):
         df = self.get_df
-        print(df.head())
         player_feature_list = [get_rolling_game_avgs(df, index_on=['bbrefID'], games=g) for g in [5,10,20]]
         opp_feature_list = [get_rolling_game_avgs(df, index_on=['opp', 'pos'], games=g) for g in [5,10,20]]
 
@@ -47,7 +46,7 @@ class BuildFeatureSet:
 
     def run(self):
         features = self.get_feature_df()
-        features.to_gbq(project=self.project,
+        features.to_gbq(project_id=self.project,
                         destination_table='{sport_type}_features_{partition_date}'.format(sport_type='basketball',
                                                                                                 partition_date=self.partition_date),
                                                                                                 if_exists="replace")
