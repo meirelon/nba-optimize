e874b523e8aed21a6b45ad14efdfc0758c6ef9fd
meirelon <nestelm@gmail.com>
2018-11-12 22:05:10 -0500

    updated main

--- main.py
+++ main.py
@@ -24,25 +24,6 @@ def test():
 													total_lineups=int(total_lineups))
     return optimize_pipeline.optimize()
 
-
-
-def start_get_data(): #make up memorable function name for cron job
-    is_cron = request.headers.get('X-Appengine-Cron', False)
-    if not is_cron:
-        return 'Bad Request', 400
-
-    try:
-        year = '2019'
-        sport_type='basketball'
-        project = 'scarlet-labs'
-        url = 'https://www.basketball-reference.com/leagues/NBA_YYYY_per_game.html'.replace('YYYYMMDD', year)
-        scraper = scrape.bbref_scrape(year=year, sport_type=sport_type, url=url, project=project)
-        scraper.run() #the actual name of the script/function you want to run contained in the subfolder
-        return "Pipeline started", 200
-    except Exception as e:
-        logging.exception(e)
-        return "Error: <pre>{}</pre>".format(e), 500
-
 @app.errorhandler(500) #error handling script for troubleshooting
 def server_error(e):
     logging.exception('An error occurred during a request.')
